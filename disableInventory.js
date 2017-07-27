d77056ea62d834acc557121b52c3040bfe30e489
DorMitzafonTaboola <dor.m@taboola.com>
2017-07-27 13:06:43 +0300

    Delete disableInventory.js

--- disableInventory.js
+++
@@ -1,6 +0,0 @@
-var inventoryCampaign = $('tbody tr:first-child');
-$(inventoryCampaign).unbind('mouseover');
-
-$(inventoryCampaign).removeAttr('id');
-$('#sort-campaign_invnentory-title').find('#selector').remove();
-$('#selector').remove();
